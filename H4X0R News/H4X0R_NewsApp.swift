47aa2dfe76d6f6596eca7e95f83ff4fc0147c421
sganas360 <sganas@caesars.com>
2023-03-29 18:07:15 -0400

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Shun Ganas on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
